84811488086b03cc4235d0b43c8f7bf751c265b3
Eesa Ali <117680696+eesaali02@users.noreply.github.com>
2024-02-28 11:57:21 -0500

    Teams Page

--- src/Teams/Teams.jsx
+++ src/Teams/Teams.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-import { useState, useContext, useEffect } from "react";
-import { Context } from "../Context";
+import { FocusableElementsInCells as TeamsData} from "./TeamsData";
+import "./Teams.css";
 
-export default function Teams(){
-    const { currentPage, setCurrentPage } = useContext(Context);
+export default function Teams() {
+  return (
+      <div>
+          <h1>Teams</h1>
+          <TeamsData />
+      </div>
+  );
+  }
 
-    useEffect(() => {
-        console.log("use effect mounted teams")
-        // fetch('/getProjects')
-        // .then(res => console.log(res));
-        setCurrentPage("teams")
-      }, []);
 
-      return (
-        <p>Teams page</p>
-      )
-}
+// import React from "react";
+// import { useState, useContext, useEffect } from "react";
+// import { Context } from "../Context";
+
+// export default function Teams(){
+//     const { currentPage, setCurrentPage } = useContext(Context);
+
+//     useEffect(() => {
+//         console.log("use effect mounted teams")
+//         // fetch('/getProjects')
+//         // .then(res => console.log(res));
+//         setCurrentPage("teams")
+//       }, []);
+
+//       return (
+//         <p>Teams page</p>
+//       )
+// }
