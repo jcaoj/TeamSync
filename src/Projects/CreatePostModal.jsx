6672e29cbe10af34816a701b5764e46708ce5d13
AvaSKress <102256067+AvaSKress@users.noreply.github.com>
2024-03-05 16:59:10 -0500

    posts in project page

---
+++ src/Projects/CreatePostModal.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useContext, useEffect } from 'react';
+import "./Projects.css";
+import "../Modal.css";
+import axios from 'axios';
+import { Button, Textarea, Title2, Input, Label, Dropdown, Option } from '@fluentui/react-components';
+import { Context } from "../Context";
+export default function CreatePostModal({ onCreate, onClose}) {
+    const [files, setFiles] = useState();
+    const [previews, setPreviews] = useState();
+    const [message, setMessage] = useState([]);
+  
+    const handleFile = (e) => {
+      setFiles(e.target.files[0]);
+    }
+    const handleUpload = () => {
+      const formdata = new FormData();
+      for (let i=0; i < files.length; i++) {
+        console.log(files[i]);
+        formdata.append('image', files[i]);
+      }
+  
+      axios.post("http://localhost:8081/uploadPost", formdata)
+      formdata.append('message', message);
+      const config = {     
+        headers: { 'content-type': 'multipart/form-data' }
+      }
+      axios.post("http://localhost:8081/uploadPost", formdata, config)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    }
+  
+    // rendering previews
+    useEffect(() => {
+      if (!files) return;
+      let tmp = [];
+      for (let i = 0; i < files.length; i++) {
+        tmp.push(URL.createObjectURL(files[i]));
+      }
+      const objectUrls = tmp;
+      setPreviews(objectUrls);
+  
+      // free memory
+      return () => {
+        for (let i = 0; i < objectUrls.length; i++) {
+          URL.revokeObjectURL(objectUrls[i]);
+        }
+      };
+    }, [files]);
+  
+    const isExistingProject = true; 
+    const existingProject = {
+      name: "Project Name",
+      team: "Project Team",
+      description: "Project Description",
+    };
+  
+    return (
+      <main className="container">
+        <br />
+        <div className="title">
+          <Title2>Post Preview</Title2>
+          </div>
+        <input
+          type="file"
+          className="fileUpload"
+          accept="image/jpg, image/jpeg, image/png"
+          multiple
+          onChange={(e) => {
+            if (e.target.files && e.target.files.length > 0) {
+              setFiles(e.target.files);
+            }
+          }}
+        />
+        {previews &&
+          previews.map((pic, index) => {
+            return <img className="postImage" key={index} src={pic} alt={`preview-${index}`} />;
+          })}
+          <div class="message">
+            <label>Message:</label>
+            <textarea
+              className="messageInput"
+              value={message}
+              onChange={(e) => setMessage([e.target.value])}
+            />
+          </div>
+        <div>
+          <Button className="uploadButton" onClick={handleUpload} appearance="primary">
+            Upload
+          </Button>
+        </div>
+      
+      </main>
+    );
+}
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -15,6 +15,7 @@ import { Button, Text, Title2,
 import { AddSquare16Regular } from "@fluentui/react-icons";
 import CreateProjectModal from "./CreateProjectModal";
 import { Context } from "../Context";
+import CreatePostModal from "./CreatePostModal";
 import axios from 'axios';
 
 export default function Projects() {
@@ -23,16 +24,25 @@ export default function Projects() {
   const [selectedProject, setSelectedProject] = useState();
   const [projects, setProjects] = useState([]);
   const [teamProjects, setTeamProjects] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isProjectModalOpen, setIsProjectModalOpen] = useState(false);
   const [projectsLoaded, setProjectsLoaded] = useState(false);
   const [teamsLoaded, setTeamsLoaded] = useState(false);
+  const [isPostModalOpen, setIsPostModalOpen] = useState(false);
 
   function createProject(project) {
     setProjects([...projects, project]); 
-    setIsModalOpen(false);
+    setIsProjectModalOpen(false);
     axios.post(`http://localhost:8081/uploadProject?teamId=${project.teamId}&name=${project.name}&description=${project.description}&status=${project.status}`)
     .then(res => console.log(res))
     .catch(err => console.log(err));
+  }
+
+  function createPost(post) {
+    setProjects([...projects, post]);
+    setIsPostModalOpen(false);
+    axios.post(`http://localhost:8081/uploadPost?projectId=${post.image}&message=${post.message}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
   function onReturnToProjects() {
@@ -76,7 +86,7 @@ export default function Projects() {
         setProjectsLoaded(true)
       })
       .catch(err => console.log(err));
-  }, [])
+  }, [teams])
 
   useEffect(() => {
     if (teams !== undefined) {
@@ -102,8 +112,8 @@ export default function Projects() {
             </MenuTrigger>
             <MenuPopover>
               <MenuList>
-                <MenuItem onClick={() => setIsModalOpen(true)}>Project</MenuItem>
-                <MenuItem>Update</MenuItem>
+                <MenuItem onClick={() => setIsProjectModalOpen(true)}>Project</MenuItem>
+                <MenuItem onClick={() => setIsPostModalOpen(true)}>Update</MenuItem>
               </MenuList>
             </MenuPopover>
           </Menu>
@@ -120,7 +130,8 @@ export default function Projects() {
       <Spinner/>
     )}
 
-    {isModalOpen && <CreateProjectModal onCreate={createProject} onClose={() => setIsModalOpen(false)} />}
+    {isProjectModalOpen && <CreateProjectModal onCreate={createProject} onClose={() => setIsProjectModalOpen(false)} />}
+    {isPostModalOpen && <CreatePostModal onCreate={createPost} onClose={() => setIsPostModalOpen(false)} />} {/* Render post modal */}
     </>
   );
 }
