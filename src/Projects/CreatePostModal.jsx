0663c5f60edad69e60ebfb04087ae5f34f1b3966
jcao1580 <jcao1580@conestogac.on.ca>
2024-03-05 19:54:52 -0500

    fix post modal styles

--- src/Projects/CreatePostModal.jsx
+++ src/Projects/CreatePostModal.jsx
@@ -2,47 +2,13 @@ import React, { useState, useContext, useEffect } from 'react';
 import "./Projects.css";
 import "../Modal.css";
 import axios from 'axios';
-import { Button, makeStyles, Textarea, Title2, Input, Label, Dropdown, Option } from '@fluentui/react-components';
+import { Button, Textarea, Title2, Input, Label, Dropdown, Option } from '@fluentui/react-components';
 import { Context } from "../Context";
-
-const useStyles = makeStyles({
-    modal: {
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        backgroundColor: 'white',
-        padding: '40px', 
-        border: '1px solid #ccc',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        width: 'auto', 
-        minWidth: '300px', 
-        maxWidth: '600px', 
-        zIndex: 1000,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '16px', 
-        boxSizing: 'border-box', 
-    },
-    input: {
-      width: '100%', 
-      marginBottom: '20px', 
-    },
-    buttonGroup: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      width: '100%', 
-      marginTop: '20px', 
-      gap: '10px', 
-    },
-  });
 
 export default function CreatePostModal({ onCreate, onClose}) {
     const [files, setFiles] = useState();
     const [previews, setPreviews] = useState();
     const [message, setMessage] = useState([]);
-    const styles = useStyles();
 
     const handleFile = (e) => {
       setFiles(e.target.files[0]);
@@ -98,9 +64,11 @@ export default function CreatePostModal({ onCreate, onClose}) {
       };
 
     return (
-      <div className={styles.modal}>
-        <br />
-          <Title2 className={styles.title}>Post Preview</Title2>
+      <div className="modal">
+          <div className="modal-content">
+            <div className="title">
+              <Title2>Post Preview</Title2>
+            </div>
           <form onSubmit={handleSubmit} style={{ width: '100%' }}>
         <input
           type="file"
@@ -120,17 +88,18 @@ export default function CreatePostModal({ onCreate, onClose}) {
           <div class="message">
             <label>Message:</label>
             <Textarea
-              className={styles.input}
+              className="input"
               value={message}
               onChange={(e) => setMessage([e.target.value])}
             />
           </div>
-        <div className={styles.buttonGroup}>
-            <Button className={styles.button} onClick={onClose}>Close</Button>
-            <Button className={styles.button} onClick={handleUpload} appearance="primary" type="submit">
+        <div className="buttonGroup">
+            <Button onClick={onClose}>Close</Button>
+            <Button onClick={handleUpload} appearance="primary" type="submit">
             Upload</Button>
         </div>
         </form>
+        </div>
       </div>
     );
 }
